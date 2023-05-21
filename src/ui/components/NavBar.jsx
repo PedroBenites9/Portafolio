f3c75794fb23f01b3ac6e8fdded3e1959785bd53
pedro benites <pedro.benites99@hotmail.com>
2023-05-21 10:17:17 -0300

    agregado desde net

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -2,26 +2,8 @@ import "./css/styleNav.css";
 import img from "../../assets/foto__perfil.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useRef, useState } from "react";
 
 export const NavBar = () => {
-  const lastScrollTop = useRef(0);
-  const [isNavbarVisible, setisNavbarVisible] = useState(true);
-
-  const handleScroll = () => {
-    const { pageYOffset } = window;
-    if (pageYOffset > lastScrollTop.current) {
-      setisNavbarVisible(false);
-    } else if (pageYOffset < lastScrollTop.current) {
-      setisNavbarVisible(true);
-    }
-    lastScrollTop.current = pageYOffset;
-  };
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll, { passive: true });
-    return window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <>
       <header>
